37fb29c1abdb1a7b00d44d349f95fb328c27e033
MariaAmor8 <m.amorocho@uniandes.edu.co>
2026-02-25 18:26:52 -0500

    ajustes IPs servidores

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.82.110.25"]
+ALLOWED_HOSTS = ["localhost", "18.232.122.229"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "23.20.13.76",  # Dirección IP de la base de datos
+        "HOST": "54.81.22.194",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.80.70.152"
+MQTT_HOST = "54.166.65.67"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
